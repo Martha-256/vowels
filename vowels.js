903535f80aab069ee15703f5e75267431670f094
mNamuli <mnamuli81@gmail.com>
2019-09-09 14:59:09 +0300

    repeated character count

--- vowels.js
+++ vowels.js
@@ -5,9 +5,22 @@ process.stdin.once('data', (string1)=>{
   var str = string1.toString();
   var vowels = str.match(/[aeiou]/g);
   let unique = [... new Set(vowels)]
-  console.log(unique.join(''));
+  var vowel = unique.join('')
+  //console.log(unique.join(''));
 
+  str2 = str.replace(/\s/g,'');
+  let result = [];
+  var repeated = str2.toLowerCase().split('').sort().join('').match(/(.)\1+/g);
   
+  if (repeated != null) {
+   repeated.forEach((elem) => {
+      result.push(elem[0]);
+    });
+  }  var count = result.length;
+  //console.log(count);
+  array_vowel = [vowel,count];
+  console.log(array_vowel);
+
 })
   
 }
